d89cdf9da2ba0e8f84f494a056ee85d9d84519f0
DebraMK <dmkochrn@outlook.com>
2022-05-15 09:10:21 -0500

    Some comments added

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require(`dotenv`).config()
 const express = require('express')
 const app = express()
 
+// Route to places
 app.use(`/places`, require(`./controllers/places`))
 
+// Route to home page
 app.get(`/`, (req, res) => {
     res.send(`Hello world! I got this`)
 })
 
+// Error page
 app.get(`*`, (req, res) => {
     res.status(404).send(`<h1>404 Page</h1>`)
 })
