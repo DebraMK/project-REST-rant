1f291bb91ff901034097cd103d45e9d7beaf89af
DebraMK <dmkochrn@outlook.com>
2022-05-18 20:58:17 -0500

    new places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: `Rizzo's Rave`,
+        city: `Merrimac`,
+        state: `WI`,
+        cuisine: `American, Persian`,
+        pic: `/images/catfood.jpg`
+    }, {
+        name: `Bayla's Bistro`,
+        city: `Baraboo`,
+        state: `WI`,
+        cuisine: `Italian, Morroccan`,
+        pic: `/images/canecorso.jpg`
+}]
