594d204dd926651be45a57bbc6006dcc873c3c84
DebraMK <dmkochrn@outlook.com>
2022-05-18 20:40:20 -0500

    testing form data

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.engine(`jsx`, require(`express-react-views`).createEngine())
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true }))
+
 // Route to places
 app.use(`/places`, placesRoutes)
 
